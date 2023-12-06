8f7bcc70b068edbf3920a080db2f66b6de8866c8
Octavia <enzo.lahaye@epsi.fr>
2023-12-06 21:58:31 +0000

    Update Renault.vue
    Update de renault.vue

--- my-vue-app/src/components/Renault.vue
+++ my-vue-app/src/components/Renault.vue
@@ -1,13 +1,16 @@
 <template>
-    <p>Liste des renaults disponibles</p>
-        <div v-for="renault in renaults" :key="renault.id"> 
-            <h3 class="card-description">{{ renault.name }}</h3>
-            <img class="card-image" :src="renault.url_img" alt="Card image">
-            <h3 class="card-description">{{ renault.description }}</h3>
-            <h3 class="card-description">{{ renault.price }}€</h3>
+    <h1 class="title">Bienvenue dans notre sélection Renault</h1>
+    <div class="cards-container">
+      <div v-for="renault in renaults" :key="renault.id" class="card"> 
+        <img class="card-image" :src="renault.url_img" alt="Image de la voiture {{ renault.name }}">
+        <div class="card-details">
+          <h2 class="card-title">{{ renault.name }}</h2>
+            <p class="card-description">{{ renault.description }}</p>
+            <p class="card-price">{{ renault.price }}€</p>
             <button>Add to cart</button>
-            <center><hr ></center>
         </div>
+      </div>
+    </div>
 </template>
 
 <style>
@@ -34,54 +37,79 @@ export default {
 
 <style scoped>
 
-hr{
-  width: 50%;
-  color:black;
-}
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100&family=Roboto+Condensed:wght@400&display=swap');
 .cards-container {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  max-width: 1200px;
+  margin: 0 auto;
 }
+
 
 
 .card {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    flex-basis: 17%;
-    margin: 10px;
-    border: 5px white;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin: 20px;
+  padding: 15px;
+  border: 2px solid #00bfff;
+  border-radius: 15px;
+  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.2);
+  background-color: #fff;
+  max-width: 300px;
+  width: 100%;
+  overflow: hidden;
+  transition: transform 0.3s ease;
 }
 
+.card:hover {
+  transform: scale(1.05);
+}
 
 .card-image {
-    width: 25%;
-    /* Ensure it takes the full width of its flex container */
-    height: 25%;
-    margin-bottom: 15px;
-    /* Set a fixed height */
-    object-fit: contain;
-    /* Maintain aspect ratio and add whitespace if needed */
-    border-radius: 10px;
-    border: 5px white;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.8);
-    background-color: white;
+  width: 100%;
+  height: 150px;
+  object-fit: cover;
+  border-radius: 10px 10px 0 0;
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.4);
 }
 
+.card-details {
+  padding: 15px;
+}
 
 .card-title {
-    flex-basis: 10%;
-    margin: 0;
-    padding: 0;
+  font-family: 'Roboto Condensed', sans-serif;
+  font-size: 20px;
+  margin-bottom: 10px;
+  color: #333;
 }
 
-
 .card-description {
-  font-family: 'Montserrat', sans-serif;
   font-family: 'Roboto Condensed', sans-serif;
-  flex-basis: 30%;
-  margin: 0;
+  margin: 10px 0;
+  color: #555;
+}
+
+.card-price {
+  font-family: 'Montserrat', sans-serif;
+  font-weight: bold;
+  color: #00bfff;
+}
+
+button {
+  background-color: #00bfff;
+  color: #fff;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+button:hover {
+  background-color: #0080ff;
 }
 </style>
