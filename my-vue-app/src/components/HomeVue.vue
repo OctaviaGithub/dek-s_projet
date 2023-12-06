9952aba2c8c1cbd617062b816e1953b3f2184819
Octavia <enzo.lahaye@epsi.fr>
2023-12-06 23:10:18 +0000

    Update Home.vue
    Update / Commit de la part de Kayleigh
    Son commit n'a pas fonctionné

--- my-vue-app/src/components/HomeVue.vue
+++ my-vue-app/src/components/HomeVue.vue
@@ -12,14 +12,15 @@
       <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="https://images.caradisiac.com/images/5/3/3/8/185338/S0-audi-a1-d-occasion-faut-il-craquer-pour-un-premier-prix-649522.jpg" class="d-block w-60" alt="...">
+      <img src="https://images-ext-1.discordapp.net/external/fB9tNDheD2nXqQ6y32X4KZ1rguxg9mb6az81p0inftw/https/images.caradisiac.com/images/5/3/3/8/185338/S0-audi-a1-d-occasion-faut-il-craquer-pour-un-premier-prix-649522.jpg?format=webp&width=1193&height=671" class="d-block w-60" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="https://images.ctfassets.net/uaddx06iwzdz/1sfLB4IrdwYgUAZBoT3phQ/5cde5764f2813e686f5fc54980099161/bmw-x4-l-02.jpg" class="d-block w-60" alt="...">
+      <img src="https://images-ext-1.discordapp.net/external/sWm_dXRojy0DGHFdS_xWKvP_EQ7qw0o4mfwGVXCufWk/https/images.bfmtv.com/_vzvLkAyJE1ZKp0q6seAJa3vnQ4%3D/0x0%3A1920x1080/1920x0/images/BMW-X1-366984.jpg?format=webp&width=1193&height=671" class="d-block w-60" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="https://photos.auto-moto.com/32/2023/05/photo_article/16558/118331/1200-L-voiture-franaise-d-occasion-moins-de-15-000-focus-sur-la-peugeot-308.webp" class="d-block w-60" alt="...">
+      <img src="https://images-ext-2.discordapp.net/external/bMmph3U_jsh0WH0_oLm6LKx1-PZZLpW5ZT1BwSlYIFA/https/www.caroom.fr/guide/wp-content/uploads/2021/10/peugeot-208-chevaux-1.jpg?format=webp&width=1193&height=671" class="d-block w-60" alt="...">
     </div>
+    fdsdsfdsfdsf
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
